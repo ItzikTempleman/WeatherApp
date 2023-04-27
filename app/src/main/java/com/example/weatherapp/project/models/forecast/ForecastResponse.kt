bb90c10cd6cdb9617ac1be4c31c2a2556bc31559
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2023-04-27 07:01:14 +0300

    x
    x

--- app/src/main/java/com/example/weatherapp/project/models/forecast/ForecastResponse.kt
+++ app/src/main/java/com/example/weatherapp/project/models/forecast/ForecastResponse.kt
@@ -1,51 +1,61 @@
-package com.example.weatherapp.project.models.forecast
+ package com.example.weatherapp.project.models.forecast
 
 import android.os.Parcelable
+import com.example.weatherapp.project.utils.Constants
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class ForecastResponse(
     @SerializedName("list")
-    val hourlyList :List<ForecastItem> = emptyList(),
+    val hourlyList: List<ForecastItem> = emptyList(),
     val city: City = City(coord = CoordinatesInForecast(0.0, 0.0))
-): Parcelable{
+) : Parcelable {
     companion object {
         fun getForecastMockObj() = ForecastResponse()
     }
 }
 
-@Parcelize
-data class ForecastItem(
-    val main: MainInForecast,
-    @SerializedName("weather")
-    val weatherInForecast: List<WeatherInForecast>,
-    @SerializedName("dt_txt")
-    val exactTime: String,
-): Parcelable
+ @Parcelize
+ data class ForecastItem(
+     val main: MainInForecast,
+     @SerializedName("weather")
+     val weatherInForecast: List<WeatherInForecast>,
+     val rain: Rain? = null,
+     @SerializedName("dt_txt")
+     val exactTime: String,
+ ) : Parcelable
+
+ @Parcelize
+ data class Rain(
+     @SerializedName("3h")
+     val threeHours: Double
+ ) : Parcelable
+
+ @Parcelize
+ data class MainInForecast(
+     val temp: Double
+ ) : Parcelable
 
 
-@Parcelize
-data class City (
-    val coord:CoordinatesInForecast
-): Parcelable
+ @Parcelize
+ data class WeatherInForecast(
+     val main: String,
+     val description: String,
+     val icon: String
+ ) : Parcelable {
+     fun getForecastImage() = Constants.WEATHER_ICON_URL + icon + Constants.WEATHER_ICON_URL_ENDING
+ }
 
 
-@Parcelize
-data class CoordinatesInForecast (
-    val lat: Double,
-    val lon: Double
-): Parcelable
-
-@Parcelize
-data class MainInForecast (
-    val temp: Double
-): Parcelable
+ @Parcelize
+ data class CoordinatesInForecast(
+     val lat: Double,
+     val lon: Double
+ ) : Parcelable
 
 
-@Parcelize
-data class WeatherInForecast(
-    val main:String,
-    val description:String,
-    val icon:String
-): Parcelable
+ @Parcelize
+ data class City(
+     val coord: CoordinatesInForecast
+ ) : Parcelable
--- app/src/main/java/com/example/weatherapp/project/view/layouts/ForecastLazyRowLayout.kt
+++ app/src/main/java/com/example/weatherapp/project/view/layouts/ForecastLazyRowLayout.kt
@@ -1,6 +1,9 @@
 package com.example.weatherapp.project.view.layouts
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -10,7 +13,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.weatherapp.R
 import com.example.weatherapp.project.models.forecast.ForecastItem
 import com.example.weatherapp.project.models.forecast.ForecastResponse
 import com.example.weatherapp.project.view.convertFromKelvinToCelsius
@@ -37,13 +48,57 @@ fun ForecastLayout(
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun ForecastItem(forecast: ForecastItem, modifier: Modifier) {
-    Column(
+    ConstraintLayout(
         modifier = modifier
+            .border(BorderStroke(0.1.dp, colorResource(id = R.color.dark_blue)))
             .width(150.dp)
     ) {
-        Text(text = convertFromKelvinToCelsius(forecast.main.temp).toInt().toString())
+        val (date, dayOfWeek, hour, degrees, degreesPercent, icon, description, precipitation) = createRefs()
+
+        val firstForecastOrNull = forecast.weatherInForecast.firstOrNull()
+        val painter = rememberImagePainter(data = firstForecastOrNull?.getForecastImage())
+
+
+        Text(
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .constrainAs(degrees) {
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                },
+            text = convertFromKelvinToCelsius(forecast.main.temp).toInt().toString(),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+
+            )
+        Text(
+            modifier = Modifier
+                .padding(top = 16.dp, start = 2.dp)
+                .constrainAs(degreesPercent) {
+                    top.linkTo(parent.top)
+                    start.linkTo(degrees.end)
+                },
+            text = "o",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+        )
+
+        Image(
+            modifier = Modifier
+                .constrainAs(icon) {
+                    top.linkTo(degrees.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                }
+                .height(50.dp)
+                .width(50.dp),
+            painter = painter,
+            contentDescription = "forecast_icon"
+        )
     }
 }
 
