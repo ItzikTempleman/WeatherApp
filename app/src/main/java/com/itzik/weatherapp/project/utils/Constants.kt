ec79a617193537a405fafc8cfe38f633338f6698
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2023-10-24 13:36:59 +0300

    h
    h

--- app/src/main/java/com/itzik/weatherapp/project/utils/Constants.kt
+++ app/src/main/java/com/itzik/weatherapp/project/utils/Constants.kt
@@ -1,13 +1,15 @@
 package com.itzik.weatherapp.project.utils
 
-object Constants  {
+object Constants {
     const val BASE_URL = "https://open-weather13.p.rapidapi.com/"
 
     const val IMAGE_BASE_URL = "https://api.unsplash.com/"
     const val IMAGE_CLIENT_ID = "0K521ZLZdskr0RQF9bmAGJemUeG7SMd_4eEc1cPirbc"
 
     const val API_KEY_NAME = "x-rapidapi-key"
-    const val API_KEY_VALUE = "370f715c6cmsh2aedaf88c0ad5bcp1aafa4jsncf895a26cb42"
+
+    //const val API_KEY_VALUE = "370f715c6cmsh2aedaf88c0ad5bcp1aafa4jsncf895a26cb42"
+    const val API_KEY_VALUE = "1efcdfbbe9mshb72149862079f25p13b0afjsne6cce1f4c14d"
 
 
     const val API_HOST_NAME = "x-rapidapi-host"
@@ -18,12 +20,11 @@ object Constants  {
     const val WEATHER_ICON_URL_ENDING = "@2x.png"
 
 
-    const val WEATHER_RESPONSE_TABLE="weatherResponseTable"
+    const val WEATHER_RESPONSE_TABLE = "weatherResponseTable"
     const val WEATHER_RESPONSE_DATABASE = "conversationDatabase"
 
 
-    const val GOOGLE_MAPS_API_KEY="AIzaSyCkfBarU6MCQRgncXXbXnT8j5Hix0ODsnA"
-
+    const val GOOGLE_MAPS_API_KEY = "AIzaSyCkfBarU6MCQRgncXXbXnT8j5Hix0ODsnA"
 
 
 }
