7065167c27ed3a82894b57c338558ffad6a2ea2c
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2023-10-28 19:43:59 +0300

    f
    f

--- app/src/main/java/com/itzik/weatherapp/project/utils/WeatherRequestInterceptor.kt
+++ app/src/main/java/com/itzik/weatherapp/project/utils/WeatherRequestInterceptor.kt
@@ -1,9 +1,9 @@
 package com.itzik.weatherapp.project.utils
 
+import com.itzik.weatherapp.BuildConfig
 import com.itzik.weatherapp.project.utils.Constants.API_HOST_NAME
 import com.itzik.weatherapp.project.utils.Constants.API_HOST_VALUE
 import com.itzik.weatherapp.project.utils.Constants.API_KEY_NAME
-import com.itzik.weatherapp.project.utils.Constants.API_KEY_VALUE
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -11,10 +11,9 @@ import okhttp3.Response
 class WeatherRequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request: Request = chain.request().newBuilder()
-            .addHeader(API_KEY_NAME, API_KEY_VALUE)
+            .addHeader(API_KEY_NAME, BuildConfig.API_KEY)
             .addHeader(API_HOST_NAME, API_HOST_VALUE)
             .build()
         return chain.proceed(request)
     }
 }
-
